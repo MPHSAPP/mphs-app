a9796d09b6dfc008c21012522696293b493f02cb
arthur <arthur.linuxpage@gmail.com>
2015-03-11 12:31:20 -0400

    Update

--- Application/src/main/java/com/example/android/customnotifications/MainActivity.java
+++ Application/src/main/java/com/example/android/customnotifications/MainActivity.java
@@ -16,20 +16,23 @@
 package com.example.android.customnotifications;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.text.Html;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import android.os.AsyncTask;
 import android.view.View;
-import android.widget.RemoteViews;
-import android.database.sqlite.SQLiteDatabase;
-import java.text.DateFormat;
-import java.util.Date;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.content.Context;
+import android.widget.ImageView;
 
 
 
@@ -47,135 +50,9 @@ import android.content.Context;
          */
 
 
-        private void createNotification() {
-            // BEGIN_INCLUDE(notificationCompat)
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-            // END_INCLUDE(notificationCompat)
 
-            // BEGIN_INCLUDE(intent)
-            //Create Intent to launch this Activity again if the notification is clicked.
-            Intent i = new Intent(this, MainActivity.class);
-            i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            PendingIntent intent = PendingIntent.getActivity(this, 0, i,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(intent);
-            // END_INCLUDE(intent)
 
-            // BEGIN_INCLUDE(ticker)
-            // Sets the ticker text
-            builder.setTicker(getResources().getString(R.string.custom_notification));
 
-            // Sets the small icon for the ticker
-            builder.setSmallIcon(R.drawable.ic_stat_custom);
-            // END_INCLUDE(ticker)
-
-            // BEGIN_INCLUDE(buildNotification)
-            // Cancel the notification when clicked
-            builder.setAutoCancel(true);
-
-            // Build the notification
-            Notification notification = builder.build();
-            // END_INCLUDE(buildNotification)
-
-            // BEGIN_INCLUDE(customLayout)
-            // Inflate the notification layout as RemoteViews
-            RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.notification);
-
-            // Set text on a TextView in the RemoteViews programmatically.
-            final String time = DateFormat.getTimeInstance().format(new Date()).toString();
-            final String text = getResources().getString(R.string.collapsed, time);
-            contentView.setTextViewText(R.id.textView, text);
-
-        /* Workaround: Need to set the content view here directly on the notification.
-         * NotificationCompatBuilder contains a bug that prevents this from working on platform
-         * versions HoneyComb.
-         * See https://code.google.com/p/android/issues/detail?id=30495
-         */
-            notification.contentView = contentView;
-
-            // Add a big content view to the notification if supported.
-            // Support for expanded notifications was added in API level 16.
-            // (The normal contentView is shown when the notification is collapsed, when expanded the
-            // big content view set here is displayed.)
-            if (Build.VERSION.SDK_INT >= 16) {
-                // Inflate and set the layout for the expanded notification view
-                RemoteViews expandedView =
-                        new RemoteViews(getPackageName(), R.layout.notification_expanded);
-                notification.bigContentView = expandedView;
-            }
-            // END_INCLUDE(customLayout)
-
-            // START_INCLUDE(notify)
-            // Use the NotificationManager to show the notification
-            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            nm.notify(0, notification);
-            // END_INCLUDE(notify)
-        }
-
-        private void Events() {
-            // BEGIN_INCLUDE(notificationCompat)
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-            // END_INCLUDE(notificationCompat)
-
-            // BEGIN_INCLUDE(intent)
-            //Create Intent to launch this Activity again if the notification is clicked.
-            Intent i = new Intent(this, MainActivity.class);
-            i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            PendingIntent intent = PendingIntent.getActivity(this, 0, i,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(intent);
-            // END_INCLUDE(intent)
-
-            // BEGIN_INCLUDE(ticker)
-            // Sets the ticker text
-            builder.setTicker(getResources().getString(R.string.button));
-
-            // Sets the small icon for the ticker
-            builder.setSmallIcon(R.drawable.ic_stat_custom);
-            // END_INCLUDE(ticker)
-
-            // BEGIN_INCLUDE(buildNotification)
-            // Cancel the notification when clicked
-            builder.setAutoCancel(true);
-
-            // Build the notification
-            Notification notification = builder.build();
-            // END_INCLUDE(buildNotification)
-
-            // BEGIN_INCLUDE(customLayout)
-            // Inflate the notification layout as RemoteViews
-            RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.notification);
-
-            // Set text on a TextView in the RemoteViews programmatically.
-            final String time = DateFormat.getTimeInstance().format(new Date()).toString();
-            final String text = getResources().getString(R.string.collapsed, time);
-            contentView.setTextViewText(R.id.textView, text);
-
-        /* Workaround: Need to set the content view here directly on the notification.
-         * NotificationCompatBuilder contains a bug that prevents this from working on platform
-         * versions HoneyComb.
-         * See https://code.google.com/p/android/issues/detail?id=30495
-         */
-            notification.contentView = contentView;
-
-            // Add a big content view to the notification if supported.
-            // Support for expanded notifications was added in API level 16.
-            // (The normal contentView is shown when the notification is collapsed, when expanded the
-            // big content view set here is displayed.)
-            if (Build.VERSION.SDK_INT >= 16) {
-                // Inflate and set the layout for the expanded notification view
-                RemoteViews expandedView =
-                        new RemoteViews(getPackageName(), R.layout.notification_expanded2);
-                notification.bigContentView = expandedView;
-            }
-            // END_INCLUDE(customLayout)
-
-            // START_INCLUDE(notify)
-            // Use the NotificationManager to show the notification
-            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            nm.notify(0, notification);
-            // END_INCLUDE(notify)
-        }
 
         @Override
         public void onBackPressed() {
@@ -185,8 +62,11 @@ import android.content.Context;
 
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            WebView webView = (WebView) findViewById(R.id.webView);
 
-            setContentView(R.layout.sample_main);
+
+
+            setContentView(R.layout.main);
         }
 
         /**
@@ -208,5 +88,68 @@ import android.content.Context;
 
         public void events(View view) {
             setContentView(R.layout.events);
+        }
+
+        public void doScale(View view){
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageResource(R.drawable.bigman);
+
+
+            Animation scaleAnimation = AnimationUtils.loadAnimation(
+                    getApplicationContext(), R.anim.scale);
+
+            imageView.startAnimation(scaleAnimation);
+
+
+
+        }
+        class RequestTask extends AsyncTask<String, String, String>{
+
+            @Override
+// username, password, message, mobile
+            protected String doInBackground(String... url) {
+                // constants
+                int timeoutSocket = 5000;
+                int timeoutConnection = 5000;
+
+                HttpParams httpParameters = new BasicHttpParams();
+                HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+                HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+                HttpClient client = new DefaultHttpClient(httpParameters);
+
+                HttpGet httpget = new HttpGet(url[0]);
+
+                try {
+                    HttpResponse getResponse = client.execute(httpget);
+                    final int statusCode = getResponse.getStatusLine().getStatusCode();
+
+                    if(statusCode != HttpStatus.SC_OK) {
+                        Log.w("MyApp", "Download Error: " + statusCode + "| for URL: " + url);
+                        return null;
+                    }
+
+                    String line = "";
+                    StringBuilder total = new StringBuilder();
+
+                    HttpEntity getResponseEntity = getResponse.getEntity();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(getResponseEntity.getContent()));
+
+                    while((line = reader.readLine()) != null) {
+                        total.append(line);
+                    }
+
+                    line = total.toString();
+                    return line;
+                } catch (Exception e) {
+                    Log.w("MyApp", "Download Exception : " + e.toString());
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                // do something with result
+            }
         }
     }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-ALL HAIL CHAIRMAN BOSCO
+Arthur: I am trying to figure out how to get content from a web page, if you have any ideas, add them to the README.md document
+
+
 ===================================
 
 
